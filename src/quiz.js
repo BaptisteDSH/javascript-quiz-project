2af76231143f287c792f8989f779668229716b84
Edvige Di Salvo <edvigedis@google.com>
2024-10-28 17:50:06 +0100

    Complete day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,57 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 4. shuffleQuestions()
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 5. checkAnswer(answer)
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 6. hasEnded()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = 0; i < this.questions.length; i++) {
+      {
+        let newQuestionsArray = [];
+        const randomIndex = Math.floor(Math.random() * this.questions.length);
+        const questionsToMove = this.questions[i];
+        this.questions.splice(i, 1);
+        this.questions.splice(randomIndex, 0, questionsToMove);
+      }
+    }
+  }
+
+  // 5. checkAnswer(answer)
+
+  checkAnswer(answer) {
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+    if (answer === currentQuestion.answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    } else {
+      return undefined;
+    }
+  }
 }
