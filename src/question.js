bae1daf9e5bc686ac954f30403b3d60334df05ba
Baptiste Desharbes <desharbesbaptiste@hotmail.fr>
2024-10-29 17:02:28 +0100

    day-2 is Done

--- src/question.js
+++ src/question.js
@@ -1,37 +1,23 @@
 class Question {
-    // YOUR CODE HERE:
+  // YOUR CODE HERE:
 
-    // 1. constructor (text, choices, answer, difficulty)
+  // 1. constructor (text, choices, answer, difficulty)
 
-    constructor(text, choices, answer, difficulty) {
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-        this.difficulty = difficulty;
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+
+  // 2. shuffleChoices()
+  shuffleChoices() {
+    let shuffledArray = [];
+    for (let i = 0; i < this.choices.length; i++) {
+      const randomIndex = Math.floor(Math.random() * this.choices.length);
+      const choiceToMove = this.choices[i];
+      this.choices.splice(i, 1);
+      this.choices.splice(randomIndex, 0, choiceToMove);
     }
-    
-    
-
-    // 2. shuffleChoices()
-    shuffleChoices() {
-        let shuffledArray = [];
-        for (let i = 0; i < this.choices.length; i++) {
-            const randomIndex = Math.floor(Math.random() * this.choices.length);
-            const choiceToMove = this.choices[i]
-            this.choices.splice(i, 1)
-            this.choices.splice(randomIndex, 0, choiceToMove)
-         
-        }
-      
-    
-
-       
-    }
+  }
 }
-
-
-      
-
-
-  
-
--- src/quiz.js
+++ src/quiz.js
@@ -54,4 +54,35 @@ class Quiz {
       return undefined;
     }
   }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty === 1 || difficulty === 2 || difficulty === 3) {
+      const filteredArray = this.questions.filter((question) => {
+        // Check if the question difficulty is valid (1, 2, or 3) and matches the provided difficulty
+        return question.difficulty === difficulty;
+      });
+
+      return (this.questions = filteredArray);
+    }
+  }
+
+  averageDifficulty() {
+    if (this.questions.length === 0) return 0; // Avoid division by zero
+    const totalOfDifficulty = this.questions.reduce((acc, curr) => {
+      return acc + curr.difficulty;
+    }, 0);
+    const averageDifficulty = totalOfDifficulty / this.questions.length;
+
+    return averageDifficulty;
+  }
 }
+
+// if (this.questions.length === 0) return 0; // Avoid division by zero
+
+//     const totalOfDifficulty = this.questions.reduce((acc, curr) => {
+//       return acc + curr.difficulty;
+//     }, 0);
+//     const averageDifficulty = totalOfDifficulty / this.questions.length;
+
+//     console.log(averageDifficulty);
+//     return averageDifficulty;
